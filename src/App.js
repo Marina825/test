b05a271a2f8e3badbf47180b24d5a6bc5cec77bd
Marina Perfileva <masamokolova@gmail.com>
2023-05-07 17:29:26 +0300

    test fir

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-
 import "./App.css";
 import "./rr.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-
         <div className="rt">my first children </div>
+        <div>lololo</div>
       </header>
     </div>
   );
